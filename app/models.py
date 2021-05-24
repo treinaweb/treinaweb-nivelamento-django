10eceb633b9ab2c0134edd91484a948051a74d4a
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2021-05-24 15:47:38 -0300

    05_Criando_Model_Migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+    STATUS_CHOICES = [
+        (1, 'A fazer'),
+        (2, 'Fazendo'),
+        (3, 'Concluido')
+    ]
+
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    data = models.DateField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS_CHOICES, null=False, blank=False)
